06f0ca9882470174127a0a7bcd6735434c69f25c
tuancuongbota <tuancuong.bota@gmail.com>
2024-03-29 22:52:26 +0700

    update home

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -10,4 +10,101 @@ $(document).ready(function(){
       autoplaySpeed: 2000,
     });
    }
+   if($('.slick-service').length) {
+    $('.slick-service').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+   }
+   if($('.slick-feedback').length) {
+    $('.slick-feedback').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+   }
+   if($('.slick-news').length) {
+    $('.slick-news').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+   }
+   $(".back-to-top").click(function (event) {
+    $('html, body').animate({scrollTop: 0}, 1000);
+    });
+    // Hide,Show ScrollToTop
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.back-to-top,.header_nav').addClass('active');
+        } else {
+            $('.back-to-top,.header_nav').removeClass('active')
+        }
+    });
+
 });
